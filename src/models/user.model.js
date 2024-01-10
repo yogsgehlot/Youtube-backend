702b1993f481cdb7aa3205b59cbbb02903fe0db9
Yogesh Gehlot <75558691+yogsgehlot@users.noreply.github.com>
2024-01-10 21:06:16 +0530

    user model with hooks and jwt, cloudenary, multer

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,4 +55,30 @@ userSchema.methods.isPasswordCorrect = async function (password){
     return await bcrypt.compare(password, this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+   return Jwt.sign(
+      {
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName,
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+      }
+    );
+}
+userSchema.methods.generateRefreshToken = function(){
+    return Jwt.sign(
+      {
+        _id: this._id
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+      }
+    );
+}
+
 export const User = mongoose.model("User", userSchema)
